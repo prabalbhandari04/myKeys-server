16c821a09101b66b17ba3a58496985389f03e01a
Prabal Bhandari <prabalb046348@gmail.com>
2022-08-14 10:37:34 +0545

    kms update delete and share and get

--- controllers/credController.js
+++ controllers/credController.js
@@ -1,14 +1,13 @@
 const {Creds} = require('../models/credModel')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-const sendMail = require('../utils/sendMail')
+const shareMail = require('../utils/shareMail')
 const {google} = require('googleapis')
 const {OAuth2} = google.auth
 const { json } = require('express')
 const mongoose = require('mongoose')
 // mailing env key
 const client = new OAuth2(process.env.MAILING_SERVICE_CLIENT_ID)
-
 // client url from dot env
 const {CLIENT_URL} = process.env
 
@@ -32,13 +31,48 @@ const userCtrl = {
 
     getCred: async (req, res) => {
         try {
+            const id = req.params.id
+            const creds = await Creds.findById(id)
+            res.json(creds)
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+
+    deleteCred : async (req, res) => {
+        try {
+            const id = req.params.id
+            const creds = await Creds.findByIdAndDelete(id)
+            res.json({msg: "Credential deleted successfully."})
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+
+    share : async (req, res) => {
+        try {
+            const id = req.params.id
+            const creds = await Creds.findById(id)
+            const email = req.body.email
+            shareMail(email, creds, "Hello, here are your credentials.")
+            res.json({msg: "Credential shared successfully.Please check "+email+" for the Credential."})
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+
+
+    findCred : async (req, res) => {
+        try {
             const creds = await Creds.find()
             res.json(creds)
         } catch (err) {
             return res.status(500).json({msg: err.message})
         }
     }
-};
+}
+
+
 
 
 
--- routes/credRouter.js
+++ routes/credRouter.js
@@ -2,7 +2,9 @@ const router = require('express').Router()
 const credCtrl = require('../controllers/credController')
 const auth = require('../middleware/auth')
 // register route user 
-router.post('/add', credCtrl.addCred)
-router.get('/get', credCtrl.getCred)
-
+router.post('/', credCtrl.addCred)
+router.get('/:id', credCtrl.getCred)
+router.delete('/:id', credCtrl.deleteCred)
+router.post('/share/:id', credCtrl.share)
+router.get('/', credCtrl.findCred)
 module.exports = router
